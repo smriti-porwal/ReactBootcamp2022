3d941ab1fccd86e9c7336b67a3215242054ba543
Smriti Porwal <smriti941144@gmail.com>
2022-11-23 23:10:20 +0530

    Created a functional component of the same using JSX

--- Assignments/src/App.js
+++ Assignments/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+// import React from "react";
 import ReactDOM from "react-dom/client";
 
 // const heading = React.createElement(
@@ -10,13 +10,16 @@ import ReactDOM from "react-dom/client";
 //     React.createElement("h3", { id: "heading3" }, "In three months"),
 //   ]
 // );
-const heading = (
+const HeadComponent = () => {
+  return (
     <div className="title">
-        <h1 id="heading1"> Namaste Javascript Bootcamp! </h1>
-        <h2 id="heading2"> From zero to hero </h2>
-        <h3 id="heading3"> In three months </h3>
+      <h1 id="heading1"> Namaste Javascript Bootcamp! </h1>
+      <h2 id="heading2"> From zero to hero </h2>
+      <h3 id="heading3"> In three months </h3>
     </div>
-)
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+// root.render(heading);
+root.render(<HeadComponent/>);
