4abd5616f27e39f97781b8f28a11dc09c8fdf422
Smriti Porwal <smriti941144@gmail.com>
2022-11-23 23:01:52 +0530

    Created the same element using JSX

--- Assignments/src/App.js
+++ Assignments/src/App.js
@@ -1,15 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
-  "div",
-  { id: "title", className: "title" },
-  [
-    React.createElement("h1", { id: "heading1" }, "Namaste Javascript Bootcamp!"),
-    React.createElement("h2", { id: "heading2" }, "From zero to hero"),
-    React.createElement("h3", { id: "heading3" }, "In three months"),
-  ]
-);
+// const heading = React.createElement(
+//   "div",
+//   { id: "title", className: "title" },
+//   [
+//     React.createElement("h1", { id: "heading1" }, "Namaste Javascript Bootcamp!"),
+//     React.createElement("h2", { id: "heading2" }, "From zero to hero"),
+//     React.createElement("h3", { id: "heading3" }, "In three months"),
+//   ]
+// );
+const heading = (
+    <div className="title">
+        <h1 id="heading1"> Namaste Javascript Bootcamp! </h1>
+        <h2 id="heading2"> From zero to hero </h2>
+        <h3 id="heading3"> In three months </h3>
+    </div>
+)
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
