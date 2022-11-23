8ba6bb15f3e4f2b68b1e67703699dddec3f960ae
Smriti Porwal <smriti941144@gmail.com>
2022-11-23 22:57:03 +0530

    Created a nested header element using React.createElement (h1, h2, h3) inside a div with class 'title'

--- Assignments/src/App.js
+++ Assignments/src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// const heading = React.createElement("h1", {className: 'heading'}, "Hello React and Parcel!");
-// component name should start with capital letter.
-const HeadComponent = () => {
-    return React.createElement("h1", {className: 'heading'}, "Hello React and Parcel!");
-};
+const heading = React.createElement(
+  "div",
+  { id: "title", className: "title" },
+  [
+    React.createElement("h1", { id: "heading1" }, "Namaste Javascript Bootcamp!"),
+    React.createElement("h2", { id: "heading2" }, "From zero to hero"),
+    React.createElement("h3", { id: "heading3" }, "In three months"),
+  ]
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(heading);
-root.render(<HeadComponent/>)
+root.render(heading);
